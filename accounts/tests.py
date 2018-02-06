073beee14a209cd9cdd6cd364aff5ceb854f3579
Safak <safakge@gmail.com>
2018-02-06 23:00:47 +0300

    Signup test cases for success and failure

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse, resolve
 
@@ -6,13 +8,70 @@ from accounts.views import signup
 
 class SignupTests(TestCase):
     def setUp(self):
-        pass
+        url = reverse('signup')
+        self.response = self.client.get(url)
 
     def test_signup_status_code(self):
-        url = reverse('signup')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_signup_url_resolves_signup_view(self):
         view = resolve('/signup')
         self.assertEqual(view.func, signup)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_for(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, UserCreationForm)
+
+
+class SuccessfulSignupTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        data = {
+            'username': 'test',
+            'password1': 'abcabcabc',
+            'password2': 'abcabcabc'
+        }
+        self.response = self.client.post(url, data)
+        self.home_url = reverse('home')
+
+    def test_redirection(self):
+        self.assertRedirects(self.response, self.home_url)
+
+    def test_user_creation(self):
+        self.assertTrue(User.objects.exists())
+
+    def test_user_authentication(self):
+        """
+        Create a new request to an arbitrary page.
+        The resulting response should now have a `user` to its context,
+        after a successful sign up.
+        """
+        url = reverse('home')
+        response = self.client.get(url)
+        user: User = response.context.get('user')
+        self.assertTrue(user.is_authenticated)
+
+
+class InvalidSignupTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.post(url, {})  # post empty data
+
+    def test_signup_status_code(self):
+        """
+        must not redirect
+        """
+        self.assertTrue(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        """
+        form must throw errors
+        """
+        form: UserCreationForm = self.response.context.get('form')
+        self.assertTrue(form.errors)
+
+    def test_no_user_creation(self):
+        self.assertFalse(User.objects.exists())
