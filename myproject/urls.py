9441facbc32bbb50b82ab9f8c7d4f452e188d757
Safak <safakge@gmail.com>
2018-02-07 12:15:47 +0300

    Logout

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from boards import views
 from accounts import views as account_views
@@ -23,6 +24,7 @@ from accounts import views as account_views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^signup/*$', account_views.signup, name='signup'),
+    url(r'^logout/*$', auth_views.LogoutView.as_view(), name='logout'),
     url(r'^about/*$', views.about, name='about'),
     url(r'^board/(?P<board_id>\d+)/*$', views.board_topics, name='board_topics'),
     url(r'^board/(?P<board_id>\d+)/new/*$', views.new_topic, name='new_topic'),
