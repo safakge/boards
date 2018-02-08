6b1da1ee010da636de0c6a79c6923db1c40745e5
Safak <safakge@gmail.com>
2018-02-08 17:18:30 +0300

    Password reset urls and (empty) template files

--- myproject/urls.py
+++ myproject/urls.py
@@ -23,11 +23,31 @@ from accounts import views as account_views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+
+    # login and logout
     url(r'^login/*$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^signup/*$', account_views.signup, name='signup'),
     url(r'^logout/*$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'^about/*$', views.about, name='about'),
+
+    # boards
     url(r'^board/(?P<board_id>\d+)/*$', views.board_topics, name='board_topics'),
     url(r'^board/(?P<board_id>\d+)/new/*$', views.new_topic, name='new_topic'),
+
+    # password reset
+    url(r'^reset/*$', auth_views.PasswordResetView.as_view(template_name='password_reset.html',
+                                                           email_template_name='password_reset_email.html',
+                                                           subject_template_name='password_reset_subject.txt'),
+        name='password_reset'),
+    url(r'^reset/done/*$', auth_views.PasswordChangeDoneView.as_view(template_name='password_reset_done.html'),
+        name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'),
+        name='password_reset_confirm'),
+    url(r'^reset/complete/*$',
+        auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+        name='password_reset_complete'),
+
+    # else
+    url(r'^about/*$', views.about, name='about'),
     path('admin/', admin.site.urls),
 ]
