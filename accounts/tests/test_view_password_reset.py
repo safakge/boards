4c08c01632d8184d59b8fc626d17de59c6589459
Safak <safakge@gmail.com>
2018-02-08 22:02:59 +0300

    Password reset complete screen, and its tests

--- accounts/tests/test_view_password_reset.py
+++ accounts/tests/test_view_password_reset.py
@@ -118,3 +118,16 @@ class PasswordResetConfirmTests(TestCase):
         """
         self.assertContains(self.response, '<input', 4)
         self.assertContains(self.response, 'type="password"', 2)
+
+
+class PasswordResetCompleteTests(TestCase):
+    def setUp(self):
+        url = reverse('password_reset_complete')
+        self.response = self.client.get(url)
+
+    def test_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_view_function(self):
+        view = resolve('/reset/complete')
+        self.assertEquals(view.func.view_class, auth_views.PasswordResetCompleteView)
