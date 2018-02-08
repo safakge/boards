1d97779f7097511e726103085b2732cd896cf31f
Safak <safakge@gmail.com>
2018-02-08 22:08:59 +0300

    Added password change and done urls and empty templates

--- myproject/urls.py
+++ myproject/urls.py
@@ -50,4 +50,11 @@ urlpatterns = [
     url(r'^reset/complete/*$',
         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
         name='password_reset_complete'),
+
+    # password change
+    url(r'^settings/password/*$', auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+        name='password_change'),
+    url(r'^settings/password/done/*$',
+        auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+        name='password_change_done'),
 ]
