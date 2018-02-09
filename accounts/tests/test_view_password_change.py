cbca17841d61323bafe87aeb7a8f455285a07951
Safak <safakge@gmail.com>
2018-02-09 12:47:21 +0300

    Password change successful change tests

--- accounts/tests/test_view_password_change.py
+++ accounts/tests/test_view_password_change.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 
@@ -11,3 +12,61 @@ class LoginRequiredPasswordChangeTests(TestCase):
         self.assertRedirects(response, f'{login_url}?next={url}')
 
 
+class PasswordChangeTestCase(TestCase):
+    def setUp(self, data={}):
+        self.user = User.objects.create_user('bill', email='a@b.com', password='abc123123')
+        self.url = reverse('password_change')
+        self.client.login(username=self.user.username, password='abc123123')
+        self.response = self.client.post(self.url, data)
+
+
+class SuccessfulPasswordChangeTests(PasswordChangeTestCase):
+    def setUp(self, **kwargs):
+        super().setUp({
+            'old_password': 'abc123123',
+            'new_password1': 'new_password',
+            'new_password2': 'new_password'
+        })
+
+    def test_redirection(self):
+        """
+        A valid form submission should redirect the user
+        """
+        self.assertRedirects(self.response, reverse('password_change_done'))
+
+    def test_password_changed(self):
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password('new_password'))
+
+    def test_user_authentication(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTrue(response.context.get('user').is_authenticated)
+
+
+class DeniedPasswordChangeTests(PasswordChangeTestCase):
+    def setUp(self, **kwargs):
+        super().setUp({
+            'old_password': 'WRONG_OLD_PASSWORD',
+            'new_password1': 'new_password',
+            'new_password2': 'new_password'
+        })
+
+    def test_status_code(self):
+        """
+        Invalid submission should not authenticate
+        """
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        form = self.response.context.get('form')
+        self.assertTrue(form.errors)
+
+    def test_didnt_change_password(self):
+        self.user.refresh_from_db()
+        self.assertTrue(not self.user.check_password('new_password'))
+
+    def test_user_not(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTrue(response.context.get('user').is_authenticated)
