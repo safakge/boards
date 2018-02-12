91a5e19b4a0cf2d4c88ab5bfc2955054654978f6
Safak <safakge@gmail.com>
2018-02-12 19:15:58 +0300

    topic.view_count is incremented every time topic_posts is called, and diplayed in board topics list

--- boards/views.py
+++ boards/views.py
@@ -46,6 +46,8 @@ def new_topic(request, board_id):
 
 def topic_posts(request, board_id, topic_id):
     topic = get_object_or_404(Topic, board_id=board_id, id=topic_id)
+    topic.view_count += 1
+    topic.save()
     return render(request, 'topic_posts.html', {'topic': topic})
 
 
