6d5b432cc9f1a1fd91df8961490a8925d291a231
Safak <safakge@gmail.com>
2018-02-12 18:22:46 +0300

    Added __str__s for models

--- boards/models.py
+++ boards/models.py
@@ -2,10 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 from typing import *
 
+from django.utils.text import Truncator
+
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Topic(models.Model):
@@ -13,6 +18,9 @@ class Topic(models.Model):
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
     starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.subject
 
 
 class Post(models.Model):
@@ -22,3 +30,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, default='No one', related_name='posts', on_delete=models.SET_DEFAULT)
     updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.SET_NULL)
+
+    def __str__(self):
+        truncated = Truncator(self.message)
+        return truncated.chars(30)
